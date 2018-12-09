b3be489e198e6b268d405973dbd3375449b5ba5c
keyne.kassapa <k.oei@uqconnect.edu.au>
2018-12-09 23:08:11 +1000

    Link VideoEnhancement and HandRecognition to main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import cv2 as cv
-from settings import logger_settings
+import numpy as np
+from matplotlib import pyplot as plt
 
 """
 TODO: A real GUI
@@ -7,7 +8,20 @@ TODO: A real GUI
 For now capture_camera_loop() just runs the camera capture frame-by-frame.
 """
 
+# ------------------ Files ------------------
+from recognition import *
+from settings import logger_settings
+
 CAMERA_LOG = logger_settings.setup_custom_logger("MAIN")
+
+# ------------------ Variables ------------------
+# lower = np.array([0,133,77], dtype="uint8")
+# upper = np.array([255,173,127], dtype="uint8")
+
+lower = np.array([0,140,0], dtype="uint8")
+upper = np.array([255,173,127], dtype="uint8")
+
+rectangle_points = [(700,0), (1200,350)]
 
 
 def capture_camera_loop():
@@ -19,17 +33,33 @@ def capture_camera_loop():
         return_val, frame = camera.read()
 
         # Colour spaces for camera output
-        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        frame_class = VideoEnhancement(
+            frame,
+            lower,
+            upper
+        )
+        frame_class.turnToYCrCb()
+        frame_class.skinExtraction()
+        frame_class.contours(1000)
+        # frame_class.frameFiltering()
+
+        hand_recognition_frame = HandRecognition(
+            frame_class.frame,
+            frame_class.original
+        )
 
         # Output Camera
-        cv.imshow('Capture', frame)
-        # cv.imshow('Capture', frame_gray)
+        cv.imshow('Capture', hand_recognition_frame.frame)
+        cv.imshow('Original', hand_recognition_frame.original)
 
         # Break loop
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
     # Quit
+    logger_settings.log_time()
     camera.release()
 
 
