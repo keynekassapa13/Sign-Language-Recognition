82b01cd92e2be281545f3c035b58377b840857e5
keyne.kassapa <k.oei@uqconnect.edu.au>
2018-12-10 15:25:05 +1000

    Update camera adjustment

--- src/main.py
+++ src/main.py
@@ -1,33 +1,64 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+import math
 
 """
 TODO: A real GUI
 
 For now capture_camera_loop() just runs the camera capture frame-by-frame.
+
+Keys:
+-----
+    q       - exit
+
 """
 
-# ------------------ Files ------------------
 from recognition import *
 from settings import logger_settings
 
 CAMERA_LOG = logger_settings.setup_custom_logger("MAIN")
 
-# ------------------ Variables ------------------
+"""
+Variables:
+----------
+
+lower - the lowest HSV skin color
+upper - the highest HSV skin color
+
+Please mind that lower and upper color threshold need to be adjusted with
+surrounding situation
+"""
+
 # lower = np.array([0,133,77], dtype="uint8")
 # upper = np.array([255,173,127], dtype="uint8")
 
 lower = np.array([0,140,0], dtype="uint8")
 upper = np.array([255,173,127], dtype="uint8")
 
-right_rectangle_points = [(700,0), (1200,350)]
-left_rectangle_points = [(0,0), (400,350)]
-
-
 def capture_camera_loop():
+
+    right_rectangle_points = [(700,0), (1200,350)]
+    left_rectangle_points = [(0,0), (400,350)]
+
     camera = cv.VideoCapture(0)
     CAMERA_LOG.info(f"Camera capture started with {camera}")
+
+    if camera.isOpened():
+        width = camera.get(3)
+        height = camera.get(4)
+
+        right_rectangle_points = [
+            ( math.floor(0.65 * width),0),
+            ( math.floor(1 * width),350)
+        ]
+        left_rectangle_points = [
+            ( math.floor(0 * width),0),
+            ( math.floor(0.35 * width),350)
+        ]
+
+        CAMERA_LOG.info(f"Left Rectangle Points {left_rectangle_points}")
+        CAMERA_LOG.info(f"Right Rectangle Points {right_rectangle_points}")
 
     while True:
         # Capture frames
@@ -39,6 +70,7 @@ def capture_camera_loop():
         # frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         # ------------------ Right Hand ------------------
+
         right_frame_class = VideoEnhancement(
             frame,
             lower,
@@ -75,7 +107,8 @@ def capture_camera_loop():
         cv.imshow('Right Hand', hand_recognition_frame.right_frame)
         cv.imshow('Original', hand_recognition_frame.original)
 
-        # Break loop
+        # ------------------ Break loop ------------------
+
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
