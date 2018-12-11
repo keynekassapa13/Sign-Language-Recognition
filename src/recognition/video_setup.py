f0ccd68446fcc0421c30b3bb5e7d2483f00cdbb0
keyne.kassapa <k.oei@uqconnect.edu.au>
2018-12-11 11:42:47 +1000

    Change cv2 to cv

--- src/recognition/video_setup.py
+++ src/recognition/video_setup.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
+import cv2 as cv
 import time
 from settings import logger_settings
 
@@ -33,43 +33,43 @@ class VideoEnhancement:
         ]
 
     def __turnToYCrCb(self):
-        self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2YCR_CB)
+        self.frame = cv.cvtColor(self.frame, cv.COLOR_BGR2YCR_CB)
 
     def backgroundSubstraction(self):
         return 0
 
     def skinExtraction(self):
-        self.frame = cv2.inRange(self.frame, self.lower, self.upper)
+        self.frame = cv.inRange(self.frame, self.lower, self.upper)
 
     def contours(self, areaNum):
-        ret, thresh = cv2.threshold(self.frame, 50, 255, cv2.THRESH_BINARY)
-        # _, contours, _ = cv2.findContours(self.frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        im2, contours, hierarchy = cv2.findContours(self.frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        ret, thresh = cv.threshold(self.frame, 50, 255, cv.THRESH_BINARY)
+        # _, contours, _ = cv.findContours(self.frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        im2, contours, hierarchy = cv.findContours(self.frame, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
         if contours:
-            areas = [cv2.contourArea(c) for c in contours]
+            areas = [cv.contourArea(c) for c in contours]
             max_index = np.argmax(areas)
             cnt = contours[max_index]
 
-            cv2.drawContours(self.frame, [cnt], -1, (0, 255, 0), 3)
+            cv.drawContours(self.frame, [cnt], -1, (0, 255, 0), 3)
 
-            M = cv2.moments(cnt)
+            M = cv.moments(cnt)
             if M["m00"] != 0:
                 cx = int(M["m10"] / M["m00"])
                 cy = int(M["m01"] / M["m00"])
             else:
                 cx, cy = 0, 0
 
-            cv2.circle(self.frame, (cx, cy), 30, (0,0,255), -1)
+            cv.circle(self.frame, (cx, cy), 30, (0,0,255), -1)
 
-            hull1 = cv2.convexHull(cnt)
+            hull1 = cv.convexHull(cnt)
 
-            cv2.drawContours(self.frame, [hull1], -1, (255, 0, 0),  1, 8)
+            cv.drawContours(self.frame, [hull1], -1, (255, 0, 0),  1, 8)
 
-            hull2 = cv2.convexHull(cnt, returnPoints=False)
+            hull2 = cv.convexHull(cnt, returnPoints=False)
 
             try:
-                defects = cv2.convexityDefects(cnt, hull2)
+                defects = cv.convexityDefects(cnt, hull2)
             except Exception as e:
                 defects = None
                 print(e)
@@ -88,26 +88,26 @@ class VideoEnhancement:
                     if (far[1] >= (cy+40)):
                         continue
 
-                    cv2.line(self.frame, end, far, (0, 100, 0), 2, 8)
+                    cv.line(self.frame, end, far, (0, 100, 0), 2, 8)
                     counter += 1
 
 
         # hull = []
         #
         # for i in range(len(contours)):
-        #     hull.append(cv2.convexHull(contours[i], False))
+        #     hull.append(cv.convexHull(contours[i], False))
         #
-        # drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
+        # drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3),snp.uint8)
         # for i, c in enumerate(contours):
-        #     area = cv2.contourArea(c)
+        #     area = cv.contourArea(c)
         #     if area > areaNum:
-        #         cv2.drawContours(self.frame, contours, i, (0, 255, 0), 3)
-        #         cv2.drawContours(self.frame, hull, i, (255, 0, 0),  1, 8)
+        #         cv.drawContours(self.frame, contours, i, (0, 255, 0), 3)
+        #         cv.drawContours(self.frame, hull, i, (255, 0, 0),  1, 8)
 
 
     def frameFiltering(self):
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-        self.frame = cv2.erode(self.frame, kernel, iterations = 2)
-        self.frame = cv2.dilate(self.frame, kernel, iterations = 2)
-        self.frame = cv2.GaussianBlur(self.frame, (3, 3), 0)
-        self.frame = cv2.bitwise_and(self.original, self.original, mask = self.frame)
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (11, 11))
+        self.frame = cv.erode(self.frame, kernel, iterations = 2)
+        self.frame = cv.dilate(self.frame, kernel, iterations = 2)
+        self.frame = cv.GaussianBlur(self.frame, (3, 3), 0)
+        self.frame = cv.bitwise_and(self.original, self.original, mask = self.frame)
