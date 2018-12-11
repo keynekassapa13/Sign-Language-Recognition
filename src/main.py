2c3da3179774070f55a302ebe1d9acb1585480ea
keyne.kassapa <k.oei@uqconnect.edu.au>
2018-12-11 16:25:13 +1000

    Clean main.py codes

--- src/main.py
+++ src/main.py
@@ -21,17 +21,14 @@ Keys:
 
 CAMERA_LOG = logger_settings.setup_custom_logger("CAMERA")
 
-"""
-Please mind that lower and upper color threshold need to be adjusted with
-surrounding situation
-"""
-
-HSV_LOWER2 = np.array([0, 140, 0], dtype="uint8")
-HSV_UPPER2 = np.array([255, 173, 127], dtype="uint8")
-
-
-# TODO: James is going to clean this up!!
-def capture_camera_loop():
+def capture_camera_loop(
+    resize=1
+):
+    """
+    Function capture_camera_loop:
+    -----------------------------
+    TODO
+    """
     camera = cv.VideoCapture(0)
     CAMERA_LOG.info(f"Camera {camera} capture started.")
 
@@ -41,38 +38,43 @@ def capture_camera_loop():
         height = camera.get(cv.CAP_PROP_FRAME_HEIGHT)
 
         right_rectangle_points = [
-            (math.floor(0.55 * width), 0),
-            (math.floor(1 * width), 350)
+            (math.floor(0.55 * width * resize), 0),
+            (math.floor(1 * width * resize), 350)
         ]
         left_rectangle_points = [
-            (math.floor(0 * width), 0),
-            (math.floor(0.45 * width), 350)
+            (math.floor(0 * width * resize), 0),
+            (math.floor(0.45 * width * resize), 350)
         ]
 
         CAMERA_LOG.debug(f"Camera Width: {width} Height: {height}")
         CAMERA_LOG.debug(f"Left Rectangle Points {left_rectangle_points}")
         CAMERA_LOG.debug(f"Right Rectangle Points {right_rectangle_points}")
 
-    # ------------------ Skin Extraction version ------------------------ #
-    hand_recognition_skin_extract(camera, width,
-                                  right_rectangle_points, left_rectangle_points)
-    # ------------------ Background subtraction version ----------------- #
-    # run_hand_segmentation(camera, (10, 100, 225, 350), 0.2)
+        hand_recognition_skin_extract(
+            camera,
+            width,
+            right_rectangle_points,
+            left_rectangle_points
+        )
+
+        # run_hand_segmentation(camera, (10, 100, 225, 350), 0.2)
 
     # Quit
     camera.release()
     CAMERA_LOG.info(f"Camera {camera} released.")
 
 
-def hand_recognition_skin_extract(camera,
-                                  width: float,
-                                  right_rectangle_points: List[Tuple[int, int]],
-                                  left_rectangle_points: List[Tuple[int, int]]):
+def hand_recognition_skin_extract(
+    camera,
+    width: float,
+    right_rectangle_points: List[Tuple[int, int]],
+    left_rectangle_points: List[Tuple[int, int]]
+):
     """
+    Function hand_recognition_skin_extract:
+    -----------------------------
     Runs skin extraction methodology.
-
-    Shows a window for left hand, right hand, and actual video capture (2x width)
-
+    Shows a window for left hand, right hand, and actual video capture
     End loop with 'q' keyboard kye.
 
     Args:
@@ -82,32 +84,40 @@ def hand_recognition_skin_extract(camera,
         right_rectangle_points: Position of left hand frame.
         left_rectangle_points: Position of left hand frame.
     """
-    # Read first frame
+    # Capture first background
     ret, frame = camera.read()
     frame = cv2.flip(frame, 1)
-    frame = imutils.resize(frame, int(width * 2))
-    right_frame_class = VideoEnhancement(frame, HSV_LOWER2, HSV_UPPER2, right_rectangle_points)
-    left_frame_class = VideoEnhancement(frame, HSV_LOWER2, HSV_UPPER2, left_rectangle_points)
+    if resize > 1:
+        frame = imutils.resize(frame, int(width * resize))
+
+    right_frame_class = VideoEnhancement(
+        frame,
+        HSV_LOWER2,
+        HSV_UPPER2,
+        right_rectangle_points
+    )
+
+    left_frame_class = VideoEnhancement(
+        frame,
+        HSV_LOWER2,
+        HSV_UPPER2,
+        left_rectangle_points
+    )
 
     while True:
         # Capture current frame from active camera.
         return_val, frame = camera.read()
         frame = cv2.flip(frame, 1)
-        frame = imutils.resize(frame, int(width * 2))
-
-        # ------------------ Right Hand ------------------
+        if resize > 1:
+            frame = imutils.resize(frame, int(width * resize))
 
         right_frame_class.set_frame(frame)
-        right_frame_class.skinExtraction()
-        right_frame_class.contours2()
-
-        # ------------------ Left Hand ------------------
+        right_frame_class.skin_extraction()
+        right_frame_class.contours_2()
 
         left_frame_class.set_frame(frame)
-        left_frame_class.skinExtraction()
+        left_frame_class.skin_extraction()
         left_frame_class.contours(1000)
-
-        # ------------------ Hand Recognition ------------------
 
         hand_recognition_frame = HandRecognition(
             left_frame_class.frame,
@@ -117,18 +127,14 @@ def hand_recognition_skin_extract(camera,
             right_rectangle_points
         )
 
-        # ------------------ Output Camera ------------------
-
         cv.imshow('Left Hand', hand_recognition_frame.left_frame)
         cv.imshow('Right Hand', hand_recognition_frame.right_frame)
         cv.imshow('Original', hand_recognition_frame.original)
-
-        # ------------------ Break loop ------------------
 
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
 
 if __name__ == '__main__':
-    capture_camera_loop()
+    capture_camera_loop(1)
     cv.destroyAllWindows()
--- src/recognition/__init__.py
+++ src/recognition/__init__.py
@@ -1,2 +1,3 @@
 from .hand_recognition import *
 from .video_setup import *
+from .hand_segment_bgsub import *
