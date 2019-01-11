e85d2e17d4ca82ce0eaf8ec6d4cfb4ae6842a57e
JNRuan <jnruan@gmail.com>
2019-01-11 12:03:45 +1000

    Script for retraining with inception model.

---
+++ tensorflow_inception/train.sh
@@ -0,0 +1,8 @@
+python -m scripts.retrain \
+  --bottleneck_dir=tf_files/bottlenecks \
+  --model_dir=tf_files/models/"${ARCHITECTURE}" \
+  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --output_graph=tf_files/retrained_graph.pb \
+  --output_labels=tf_files/retrained_labels.txt \
+  --image_dir=tf_files/flower_dataset
+
