bb29b2c6ea9b2b3d9b234870a0ffd86f07ea43fe
JNRuan <jnruan@gmail.com>
2019-01-14 11:56:56 +1000

    Moved inception retraining and classification to src, and named package explicitly as demo.

--- tensorflow_inception/train.sh
+++
@@ -1,8 +0,0 @@
-python3 scripts/retrain.py \
-  --bottleneck_dir=tf_files/bottlenecks \
-  --how_many_training_steps=4000 \
-  --model_dir=tf_files/models \
-  --summaries_dir=tf_files/training_summaries \
-  --output_graph=tf_files/retrained_graph.pb \
-  --output_labels=tf_files/retrained_labels.txt \
-  --image_dir=flower_dataset
