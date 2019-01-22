ac3d37f643cb8f73732758bb50f7fe79f0afac12
keyne.kassapa <k.oei@uqconnect.edu.au>
2019-01-22 15:15:58 +1000

    Connecting tensorflow with main py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
+import tensorflow as tf
+import sys
 import cv2 as cv
-# import pyrealsense2 as rs
 import imutils
 import math
 import numpy as np
-import tensorflow as tf
-import sys
 import os
 from typing import List, Tuple
 
@@ -25,6 +24,17 @@ Keys:
 """
 
 CAMERA_LOG = logger_settings.setup_custom_logger("CAMERA")
+CAMERA_LOG.debug(f"Tensorflow {tf.__version__}")
+
+label_lines = [line.rstrip() for line in tf.gfile.GFile("mllib/tf_files/retrained_labels.txt")]
+
+with tf.gfile.GFile("mllib/tf_files/retrained_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(
+        graph_def,
+        name=''
+    )
 
 def setup_camera(
     resize=1
@@ -81,58 +91,70 @@ def hand_recognition(
         left_rectangle_points: Position of left hand frame.
     """
     # Capture first background
-    _, frame = camera.read()
-    frame = cv.flip(frame, 1)
-    if resize:
-        frame = imutils.resize(frame, int(width * resize))
+    with tf.Session() as sess:
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
-    right_frame_class = VideoEnhancement(
-        frame,
-        HSV_LOWER2,
-        HSV_UPPER2,
-        right_rectangle_points
-    )
-
-    left_frame_class = VideoEnhancement(
-        frame,
-        HSV_LOWER2,
-        HSV_UPPER2,
-        left_rectangle_points
-    )
-
-    while True:
-        # Capture current frame from active camera.
         _, frame = camera.read()
         frame = cv.flip(frame, 1)
         if resize:
             frame = imutils.resize(frame, int(width * resize))
 
-        right_frame_class.set_frame(frame)
-        right_frame_class.skin_extraction()
-        right_frame_class.contours(CONTOURS_AREA_THRESH)
-
-        left_frame_class.set_frame(frame)
-        left_frame_class.skin_extraction()
-        left_frame_class.contours(CONTOURS_AREA_THRESH)
-
-        hand_recognition_frame = HandRecognition(
-            left_frame_class.frame,
-            right_frame_class.frame,
+        right_frame_class = VideoEnhancement(
             frame,
-            left_rectangle_points,
+            HSV_LOWER2,
+            HSV_UPPER2,
             right_rectangle_points
         )
 
-        cv.imshow('Left Hand', hand_recognition_frame.left_frame)
-        cv.imshow('Right Hand', hand_recognition_frame.right_frame)
-        cv.imshow('Original', hand_recognition_frame.original)
+        left_frame_class = VideoEnhancement(
+            frame,
+            HSV_LOWER2,
+            HSV_UPPER2,
+            left_rectangle_points
+        )
 
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+        while True:
+            # Capture current frame from active camera.
+            _, frame = camera.read()
+            frame = cv.flip(frame, 1)
+            if resize:
+                frame = imutils.resize(frame, int(width * resize))
 
-    # Quit
-    camera.release()
-    CAMERA_LOG.info(f"Camera {camera} released.")
+            right_frame_class.set_frame(frame)
+            right_frame_class.skin_extraction()
+            right_frame_class.contours(CONTOURS_AREA_THRESH)
+
+            predictions = sess.run(softmax_tensor, {'Cast:0': right_frame_class.frame})
+
+            top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+            for node_id in top_k:
+                human_string = label_lines[node_id]
+                score = predictions[0][node_id]
+                print('%s (score = %.5f)' % (human_string, score))
+
+            print('------------------ \n\n')
+
+            left_frame_class.set_frame(frame)
+            left_frame_class.skin_extraction()
+            left_frame_class.contours(CONTOURS_AREA_THRESH)
+
+            hand_recognition_frame = HandRecognition(
+                left_frame_class.frame,
+                right_frame_class.frame,
+                frame
+            )
+
+            cv.imshow('Left Hand', hand_recognition_frame.left_frame)
+            cv.imshow('Right Hand', hand_recognition_frame.right_frame)
+            cv.imshow('Original', hand_recognition_frame.original)
+
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Quit
+        camera.release()
+        CAMERA_LOG.info(f"Camera {camera} released.")
 
 
 if __name__ == '__main__':
--- src/recognition/hand_recognition.py
+++ src/recognition/hand_recognition.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
+import cv2 as cv
 
 class HandRecognition:
 
@@ -8,35 +8,8 @@ class HandRecognition:
         self,
         left_frame,
         right_frame,
-        original,
-        left_rectangle,
-        right_rectangle
+        original
     ):
         self.left_frame = left_frame
         self.right_frame = right_frame
         self.original = original
-        self.left_rectangle = left_rectangle
-        self.right_rectangle = right_rectangle
-        self.__rectangle()
-        self.makePoints()
-
-    def makePoints(self):
-        # TO DO
-        return None
-
-    def __rectangle(self):
-        self.original = cv2.rectangle(
-            self.original,
-            (self.left_rectangle[0][0],self.left_rectangle[0][1]),
-            (self.left_rectangle[1][0],self.left_rectangle[1][1]),
-            (0,255,0),
-            3
-        )
-
-        self.original = cv2.rectangle(
-            self.original,
-            (self.right_rectangle[0][0],self.right_rectangle[0][1]),
-            (self.right_rectangle[1][0],self.right_rectangle[1][1]),
-            (0,255,0),
-            3
-        )
