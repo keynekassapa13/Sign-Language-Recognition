14fb7fbe6a837657b202fee527fada3dd819f1fa
keyne.kassapa <k.oei@uqconnect.edu.au>
2019-01-25 13:48:22 +1000

    Cleaning rectangles

--- src/recognition/hand_recognition.py
+++ src/recognition/hand_recognition.py
@@ -14,8 +14,10 @@ class HandRecognition:
         self.left_frame = left_frame
         self.right_frame = right_frame
         self.original = original
+        self.show_original = original
 
         self.all_rectangles = None
+        self.ml_predictions = {}
 
         self.left_rectangle = left_frame.rectangle
         self.right_rectangle = right_frame.rectangle
@@ -24,33 +26,38 @@ class HandRecognition:
         self.printResult()
 
     def printResult(self):
-        self.__printText(self.left_frame.frame, str(self.left_frame.final_count))
-        self.__printText(self.right_frame.frame, str(self.right_frame.final_count))
+        self.__printText(
+            self.left_frame.frame,
+            str(self.left_frame.final_count)
+        )
+        self.__printText(
+            self.right_frame.frame,
+            str(self.right_frame.final_count)
+        )
 
     def classification(self):
         return
 
     def __rectangle(self):
-        self.original = cv.rectangle(
-            self.original,
+        self.all_rectangles = self.original[
+            self.rectangle_points[0][1]:self.rectangle_points[1][1],
+            self.rectangle_points[0][0]:self.rectangle_points[1][0]
+        ]
+        self.show_original = cv.rectangle(
+            self.show_original,
             (self.left_rectangle[0][0],self.left_rectangle[0][1]),
             (self.left_rectangle[1][0],self.left_rectangle[1][1]),
             (0,255,0),
             3
         )
 
-        self.original = cv.rectangle(
-            self.original,
+        self.show_original = cv.rectangle(
+            self.show_original,
             (self.right_rectangle[0][0],self.right_rectangle[0][1]),
             (self.right_rectangle[1][0],self.right_rectangle[1][1]),
             (0,255,0),
             3
         )
-
-        self.all_rectangles = self.original[
-            self.rectangle_points[0][1]:self.rectangle_points[1][1],
-            self.rectangle_points[0][0]:self.rectangle_points[1][0]
-        ]
 
     def __printText(self, frame, text):
         try:
